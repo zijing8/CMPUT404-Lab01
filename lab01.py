24cb72f5c59c2cf0f42929538141c9e192a05233
Sam Lu <zijing8@cs.ualberta.ca>
2023-09-13 18:35:39 -0600

    step 10

--- lab01.py
+++ lab01.py
@@ -2,5 +2,5 @@ import requests
 
 #print("requests libary version is: " + requests.__version__) 
 
-resp = requests.get("http://google.com/")
-print(resp.txt)
+resp = requests.get("https://raw.githubusercontent.com/zijing8/CMPUT404-Lab01/master/lab01.py")
+print(resp.text)
